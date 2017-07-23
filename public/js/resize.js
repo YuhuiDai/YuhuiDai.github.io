543f244a02c3167fcadbdb56c051c705e49dc4a6
Yuhui_Dai <yd610@nyu.edu>
2017-07-23 17:04:40 +0400

    added

---
+++ public/js/resize.js
@@ -0,0 +1,60 @@
+
+$(document).ready(function(){
+	resizeDiv();
+	$("#coding").hover(
+		
+		function(){
+			changeToHidden('bubbleExplosion');
+			changeToHidden('sankeyChart');
+			changeToHidden('trianglify');
+			changeToVisible("matrix");
+
+		}
+	);
+	$("#data_visualization").hover(
+		
+		function(){
+			changeToHidden('bubbleExplosion');
+			changeToHidden('trianglify');
+			changeToHidden("matrix");
+			changeToVisible('sankeyChart');
+		}
+	);
+	$("#product_design").hover(
+		
+		function(){
+			changeToHidden('sankeyChart');
+			changeToHidden('bubbleExplosion');
+			changeToHidden('matrix');
+			changeToVisible("trianglify");
+			
+		}
+	);
+// document.ready finish
+});
+
+window.onresize = function(event) {
+	resizeDiv();
+}
+
+function resizeDiv() {
+	
+	$(".bubbleChart").css("max-width", parseInt($(window).innerWidth())*0.5 + "px");	
+}
+
+
+function changeToVisible(id) {
+    var x = document.getElementById(id);
+    if (x.style.visibility == 'hidden') {
+        $("#"+id).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}, 1200);
+    } 
+}
+
+function changeToHidden(id) {
+    var x = document.getElementById(id);
+    if (x.style.visibility == 'visible') {
+        x.style.visibility = 'hidden';
+    }
+}
+
+
